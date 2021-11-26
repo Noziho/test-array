b9b3bee5d64c4be0cc52bbc77c4fef6b139bfdac
Noah2211 <noah.decroix3@gmail.com>
2021-11-26 09:17:13 +0100

    test Math

---
+++ app.js
@@ -0,0 +1,7 @@
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let c = document.getElementById("c");
+
+a.innerHTML = Math.floor(a.innerHTML);
+b.innerHTML = Math.round(b.innerHTML);
+c.innerHTML = Math.ceil(c.innerHTML);
